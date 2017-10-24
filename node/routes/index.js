5659f66244b592cb3800bcc3f5e8787abe3a8a96
Anita <anitageorgev@gmail.com>
2017-10-24 17:02:15 -0500

    Changes to node api

---
+++ node/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const util = require('util');
+
+const config = {
+    user: '',
+    password : '',
+    server : ''
+}
--- node/server.js
+++ node/server.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/docs"));
-
 app.use("/", index);
 
 app.get("/",(req,res) => {
