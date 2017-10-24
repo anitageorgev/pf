8675e744deb1a6f9ab429d1d635f0fb65be49c24
Anita <anitageorgev@gmail.com>
2017-10-24 16:41:55 -0500

    improved server.js

--- node/server.js
+++ node/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 
+var index = require(".routes/index");
+
 var app = express();
 
 app.use(cors());
@@ -9,3 +11,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/docs"));
 
+app.use("/", index);
+
+app.get("/",(req,res) => {
+    res.sendFile("index.html");
+});
+
+app.get("*", (req, res) => {
+    res.status(404).send("404 Error: Resource requested does not exist.");
+});
+
+app.listen(PORT, () =>{
+    console.log("Server started on port: "+ PORT);
+});
+
+module.exports= app;
